a5a837597199433cff7cb5696b5ca76a48c4ddc0
Emmi_Official <144928805+EmmiOfficial@users.noreply.github.com>
2025-02-12 14:50:18 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import VTuberController from './VTuberController';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <VTuberController />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
